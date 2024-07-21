82cde7210fffa0ec86aabab37c4904974be17bb7
Máté Homolya <mate.homolya@gmail.com>
2024-07-21 10:11:43 -0700

    Add verify function to main python api client

--- client.py
+++ client.py
@@ -41,10 +41,11 @@ class TrendQuery(BaseModel):
 
 class ApiClient:
     def __init__(
-        self, base_url: str = "https://api.crypticorn.com", api_key: str = None
+        self, base_url: str = "https://api.crypticorn.com", api_key: str = None, token: str = None
     ):
         self.base_url = base_url
         self.api_key = api_key
+        self.token = token
         self.client = httpx.Client()
 
     def get_response(
@@ -304,6 +305,20 @@ class ApiClient:
             headers={"Authorization": f"Bearer {self.api_key}"},
         )
         return DataFrame(response.json())
+    
+    def verify(self, token: str|None = None) -> bool:
+        if token is None:
+            token = self.token
+        response = self.client.get(
+            self.base_url + "/trpc/verify",
+            headers={"Authorization": f"Bearer {token}"},
+        )
+        response.raise_for_status()
+        try:
+            res = response.json()
+            return res['result']['data']['json']
+        except:
+            return None
 
 # testing
 if __name__ == "__main__":
