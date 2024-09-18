f7eee6a62f20f539e2b6874ef7c8071fe2c82dd3
Timon Rieger <timon@riegerx.de>
2024-09-18 16:37:23 +0200

    fixes

--- tests/test_hive_client.py
+++ tests/test_hive_client.py
@@ -13,27 +13,30 @@ if __name__ == "__main__":
 
     print("Testing create_account:")
     #print(client.create_account("test_user1"))
-    #
-    print("\nTesting get_account_info:")
-    #response = client.get_account_info()
-    #print(response)
 
+    print("\nTesting update_username")
+    #print(client.update_username("Mister Berta"))
     #
-    print("\nTesting create_model:")
-    #print(client.create_model(1, "Tatooine"))
-    #
+    # for i in range(10):
+    # #
+    #     print("\nTesting create_model:")
+    #     print(client.create_model(random.choice(client.coins), random.choice(client.targets)))
+    #     #
+    #     print("\nTesting evaluate_model:")
+    #     data = pd.DataFrame(columns=["feature1"], data=[random.gauss(1,0.02) for _ in range(4000)])
+    #     print(client.evaluate_model(i, data))
     print("\nTesting evaluate_model:")
     #data = pd.DataFrame(columns=["feature1"], data=[random.gauss(1,0.02) for _ in range(4000)])
-    #print(client.evaluate_model(1, data))
+    #rint(client.evaluate_model(2, data))
 
     print("\nTesting help:")
     #print(client.help())
 
-    print("\nTesting get_all_models:")
-    #print(client.get_models())
-
     print("\nTesting get_specific_model:")
-    #print(client.get_models(1))
+    #print(client.get_model(8))
+
+    print("\nTesting get_leaderboard:")
+    print(client.get_leaderboard())
 
     print("\nTesting generate_api_key:")
     #print(client.generate_api_key())
@@ -48,4 +51,7 @@ if __name__ == "__main__":
     #print(client.update_username("Mister Y"))
 
     print("\nTesting download_data:")
-    print(client.download_data(model_id=1, feature_size="small"))
+    #print(client.download_data(model_id=1, feature_size="small"))
+
+    print("\nTesting get_account_info:")
+    #print(client.get_account_info(username="Mister Berta"))
