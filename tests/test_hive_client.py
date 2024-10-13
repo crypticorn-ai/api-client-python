f601d83d2a26156bf4c1d7580f4d890712b90514
Timon Rieger <timon@riegerx.de>
2024-10-13 15:25:19 +0200

    prepare pip package release v0.2.0

--- tests/test_hive_client.py
+++ tests/test_hive_client.py
@@ -1,4 +1,4 @@
-from python.client import HiveClient
+from api_client.python.client import HiveClient
 import random
 import pandas as pd
 
@@ -9,23 +9,23 @@ import pandas as pd
 # user_id = response["sub"]
 
 if __name__ == "__main__":
-    client = HiveClient(token="your_bearer_token_here")
-
+    client = HiveClient(token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJuYlowNUVqS2ZqWGpXdDBTMDdvOSIsImF1ZCI6ImFwcC5jcnlwdGljb3JuLmNvbSIsImlzcyI6ImFjY291bnRzLmNyeXB0aWNvcm4uY29tIiwianRpIjoicTdQMVh3WG9MdU5wbEJJNlFMVWMiLCJpYXQiOjE3Mjg4MTk1MDAsImV4cCI6MTcyODgyMzEwMCwic2NvcGVzIjpbInJlYWQ6cHJlZGljdGlvbnMiXX0.zbV9fQUmIQ1yYwaaAHazc1eYm6kK9-YMorOcWyWiuYg")
+    
     print("Testing create_account:")
-    #print(client.create_account("test_user1"))
+    #print(client.create_account("test_user12"))
 
     #
     # for i in range(10):
     # #
     print("\nTesting create_model:")
-    #print(client.create_model(random.choice(client.coins), random.choice(client.targets)))
+    #print(client.create_model(1, "Tatooine"))
     #     #
     #     print("\nTesting evaluate_model:")
     #     data = pd.DataFrame(columns=["feature1"], data=[random.gauss(1,0.02) for _ in range(4000)])
     #     print(client.evaluate_model(i, data))
     print("\nTesting evaluate_model:")
-    #data = pd.DataFrame(columns=["feature1"], data=[random.gauss(1,0.02) for _ in range(4000)])
-    #rint(client.evaluate_model(2, data))
+    #data = pd.DataFrame(columns=["feature1"], data=[random.gauss(1,0.02) for _ in range(4986)])
+    #print(client.evaluate_model(1, data))
 
     print("\nTesting help:")
     #print(client.help())
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     #print(client.get_leaderboard())
 
     print("\nTesting generate_api_key:")
-    print(client.generate_api_key())
+    #print(client.generate_api_key())
 
     print("\nTesting delete_api_key:")
     #print(client.delete_api_key())
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     #print(client.update_username("Mister Y"))
 
     print("\nTesting download_data:")
-    #print(client.download_data(model_id=1, feature_size="small", version=2))
+    print(client.download_data(model_id=1, feature_size="small", version=1))
 
     print("\nTesting get_account_info:")
-    #print(client.get_account_info(username="test_user1"))
+    #print(client.get_account_info(username="Mister Y"))
