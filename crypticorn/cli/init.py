f555ed3bfef151c3fe8f9e6c0b83b9b2660ccc4e
timonrieger <mail@timonrieger.de>
2025-04-09 22:36:46 +0200

    add auth.py template and output and force options

--- crypticorn/cli/init.py
+++ crypticorn/cli/init.py
@@ -28,16 +28,48 @@ def init_group():
     pass
 
 @init_group.command("ruff")
-def init_ruff():
+@click.option('-f', '--force', is_flag=True, help='Force overwrite the ruff.yml')
+def init_ruff(force):
     """Add .github/workflows/ruff.yml"""
     root = get_git_root()
     target = root / ".github/workflows/ruff.yml"
+    if target.exists() and not force:
+        click.secho("File already exists, use --force / -f to overwrite", fg="red")
+        return
     copy_template("ruff.yml", target)
 
 @init_group.command("docker")
-def init_docker():
+@click.option('-o', '--output', type=click.Path(), help='Custom output path for the Dockerfile')
+@click.option('-f', '--force', is_flag=True, help='Force overwrite the Dockerfile')
+def init_docker(output, force):
     """Add Dockerfile"""
     root = get_git_root()
-    target = root / "Dockerfile"
+    target = Path(output) if output else root / "Dockerfile"
+    if target.exists() and not force:
+        click.secho("File already exists, use --force / -f to overwrite", fg="red")
+        return
     copy_template("Dockerfile", target)
     click.secho("Make sure to update the Dockerfile", fg="yellow")
+
+@init_group.command("auth")
+@click.option('-o', '--output', type=click.Path(), help='Custom output path for the auth handler')
+@click.option('-f', '--force', is_flag=True, help='Force overwrite the auth handler')
+def init_auth(output, force):
+    """Add auth.py with auth handler. Everything you need to start using the auth service."""
+    root = get_git_root()
+    if Path(output).is_dir():
+        click.secho("Output path is a directory, please provide a file path", fg="red")
+        return
+    target = Path(output) if output else root / "src/auth.py"
+    if target.exists() and not force:
+        click.secho("File already exists, use --force / -f to overwrite", fg="red")
+        return
+    copy_template("auth.py", target)
+    click.secho('''
+    Make sure to update the .env file with:
+        IS_DOCKER=0
+        API_ENV=local
+    and the docker-compose.yml file with:
+        environment:
+            - IS_DOCKER=1
+    ''', fg="yellow")
