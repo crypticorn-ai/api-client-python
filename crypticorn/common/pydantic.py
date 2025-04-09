4a1446c8ca09a5f31d243310aa045ae49e7acc91
timonrieger <mail@timonrieger.de>
2025-04-10 00:24:56 +0200

    fix: refactor generated model name by partial_model and add to init file

--- crypticorn/common/pydantic.py
+++ crypticorn/common/pydantic.py
@@ -6,8 +6,8 @@ from pydantic.fields import FieldInfo
 
 
 def partial_model(model: Type[BaseModel]) -> Type[BaseModel]:
-    '''Marks all fields of a model as optional. Useful for updating models.
-    
+    """Marks all fields of a model as optional. Useful for updating models.
+
     >>> @partial_model
     >>> class Model(BaseModel):
     >>>     i: int
@@ -15,18 +15,22 @@ def partial_model(model: Type[BaseModel]) -> Type[BaseModel]:
     >>>     s: str
 
     >>> Model(i=1)
-    '''
-    def make_field_optional(field: FieldInfo, default: Any = None) -> Tuple[Any, FieldInfo]:
+    """
+
+    def make_field_optional(
+        field: FieldInfo, default: Any = None
+    ) -> Tuple[Any, FieldInfo]:
         new = deepcopy(field)
         new.default = default
         new.annotation = Optional[field.annotation]  # type: ignore
-        return new.annotation, new 
+        return new.annotation, new
+
     return create_model(
-        f'Partial{model.__name__}',
+        model.__name__,
         __base__=model,
         __module__=model.__module__,
         **{
             field_name: make_field_optional(field_info)
             for field_name, field_info in model.model_fields.items()
-        }
+        },
     )
