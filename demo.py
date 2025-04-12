393c5612093c1194c989bedaaf869a1df5d3b581
timonrieger <mail@timonrieger.de>
2025-04-12 23:55:54 +0200

    bump openapi-generator-cli to v7.12.0

--- demo.py
+++ demo.py
@@ -14,9 +14,7 @@ jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJuYlowNUVqS2ZqWGpXdDBTMDdv
 
 
 async def main():
-    async with ApiClient(
-        base_url=BaseUrl.LOCAL, api_key=""
-    ) as client:
+    async with ApiClient(base_url=BaseUrl.LOCAL, api_key="") as client:
         # json response
         # response = await client.pay.products.get_products_without_preload_content()
         # print(10 * "=" + "This is the raw json response" + 10 * "=")
