9522156d71e80db83a0c0e672e1007fd1e00687c
timonrieger <mail@timonrieger.de>
2025-04-25 21:13:56 +0200

    feat: add custom Http Exception class with shared error codes

--- crypticorn/common/exceptions.py
+++ crypticorn/common/exceptions.py
@@ -13,14 +13,21 @@ class HTTPException(FastAPIHTTPException):
     def __init__(
         self,
         status_code: int,
-        detail: ExceptionDetail,
+        detail: ExceptionDetail | str,
         headers: Optional[Dict[str, str]] = None,
     ):
-        exc = (
-            ExceptionDetail(**detail)
-            if not isinstance(detail, ExceptionDetail)
-            else detail
-        )
+        try:
+            exc = (
+                ExceptionDetail(**detail)
+                if not isinstance(detail, ExceptionDetail)
+                else detail
+            )
+        except Exception as e:
+            exc = ExceptionDetail(
+                message=detail,
+                error=ApiError.UNKNOWN_ERROR,
+            )
+
         super().__init__(
             status_code=status_code, detail=exc.model_dump(), headers=headers
         )
