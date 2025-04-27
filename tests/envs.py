09915d8dcd1d0004580321dd2ccef2f972dd7919
timonrieger <mail@timonrieger.de>
2025-04-27 20:18:18 +0200

    test: add jwt generation function

--- tests/envs.py
+++ tests/envs.py
@@ -1,11 +1,33 @@
 import os
 from dotenv import load_dotenv
+from crypticorn.common import gen_random_id
+import jwt
+import time
 
 load_dotenv(dotenv_path=".env.test")
 
+def generate_valid_jwt():
+    now = int(time.time())
+    payload = {
+        "sub": USER_ID,
+        "aud": JWT_AUDIENCE,
+        "iss": JWT_ISSUER,
+        "jti": gen_random_id(),
+        "iat": now,
+        "exp": now + JWT_EXPIRES_IN,
+        "scopes": [],
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm="HS256")
+    return token
+
 # JWT
+JWT_SECRET = os.getenv("JWT_SECRET")
+JWT_ISSUER = os.getenv("JWT_ISSUER")
+JWT_AUDIENCE = os.getenv("JWT_AUDIENCE")
+JWT_EXPIRES_IN = 60 * 60  # 1 hour
+USER_ID = os.getenv("USER_ID")
 EXPIRED_JWT = os.getenv("EXPIRED_JWT")
-VALID_JWT = os.getenv("VALID_JWT")
+VALID_JWT = generate_valid_jwt()
 # API KEY
 FULL_SCOPE_API_KEY = os.getenv("FULL_SCOPE_API_KEY")
 ONE_SCOPE_API_KEY = os.getenv("ONE_SCOPE_API_KEY")
