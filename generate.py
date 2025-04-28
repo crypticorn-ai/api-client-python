6f9053d88a0ccb423cad095463d6d9b6c5b31b1d
timonrieger <mail@timonrieger.de>
2025-04-28 11:57:11 +0200

    build: clean generated code before regenerating

--- generate.py
+++ generate.py
@@ -70,6 +70,10 @@ def main():
         print(f"Failed to connect to API for {module_name} module")
         sys.exit(1)
 
+    print("Deleting existing packages's client and docs folders")
+    subprocess.run(["rm", "-rf", f"python/crypticorn/{module_name}/client"], check=True)
+    subprocess.run(["rm", "-rf", f"python/crypticorn/{module_name}/docs"], check=True)
+
     print(
         f"Generating {module_name} client using {ROOT_URL}/{module_name}/openapi.json"
     )
