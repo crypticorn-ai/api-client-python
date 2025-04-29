4f622b70135ad46a1c0802e01bde89671743ec06
timonrieger <mail@timonrieger.de>
2025-04-29 17:02:00 +0200

    update sentiment client

--- crypticorn/common/mixins.py
+++ crypticorn/common/mixins.py
@@ -43,22 +43,6 @@ class ExcludeEnumMixin:
         return schema
 
 
-class ScopeFallback(EnumMeta):
-    """Fallback to no scope for unknown scopes."""
-
-    # Note: This is a workaround to avoid the AttributeError when an unknown scope is accessed.
-    # As soon as we have stable scopes, we can remove this.
-
-    def __getattr__(cls, name):
-        # Let Pydantic/internal stuff pass silently ! fragile
-        if name.startswith("__"):
-            raise AttributeError(name)
-        logger.warning(
-            f"Unknown scope '{name}' - falling back to no scope - update crypticorn package or check for typos"
-        )
-        return None
-
-
 class ApiErrorFallback(EnumMeta):
     """Fallback for enum members that are not yet published to PyPI."""
 
