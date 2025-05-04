ca0da427eb98b59789e871e1c04c7b06cba7f92b
timonrieger <mail@timonrieger.de>
2025-05-05 00:49:37 +0200

    fix: run logging configuration in init of the library

--- crypticorn/__init__.py
+++ crypticorn/__init__.py
@@ -3,6 +3,10 @@
 .. include:: ../CHANGELOG.md
 """
 
+from crypticorn.common.logging import configure_logging
+configure_logging("crypticorn")
+
 from crypticorn.client import ApiClient
 
 __all__ = ["ApiClient"]
+
