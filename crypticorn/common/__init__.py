4b9eb6ec43ff321408d57c5717bef03a25735751
timonrieger <mail@timonrieger.de>
2025-05-04 23:48:13 +0200

    feat: configure formatted logging

--- crypticorn/common/__init__.py
+++ crypticorn/common/__init__.py
@@ -8,4 +8,7 @@ from crypticorn.common.enums import *
 from crypticorn.common.utils import *
 from crypticorn.common.exceptions import *
 from crypticorn.common.pagination import *
-from crypticorn.common.status_router import router as status_router
+from crypticorn.common.logging import *
+from crypticorn.common.ansi_colors import *
+from crypticorn.common.router.status_router import router as status_router
+from crypticorn.common.router.admin_router import router as admin_router
