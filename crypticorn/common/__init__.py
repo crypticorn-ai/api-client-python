db4e6e1b4b524758c62a46bd218399d275c89a6a
timonrieger <mail@timonrieger.de>
2025-05-08 15:02:22 +0200

    feat: provide default openapi tags from the status and admin router

--- crypticorn/common/__init__.py
+++ crypticorn/common/__init__.py
@@ -12,5 +12,6 @@ from crypticorn.common.logging import *
 from crypticorn.common.ansi_colors import *
 from crypticorn.common.middleware import *
 from crypticorn.common.warnings import *
+from crypticorn.common.openapi import *
 from crypticorn.common.router.status_router import router as status_router
 from crypticorn.common.router.admin_router import router as admin_router
