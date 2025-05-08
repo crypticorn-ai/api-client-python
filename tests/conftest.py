055d2cc58e3d050ee1fe439c496adf088c66fa64
timonrieger <mail@timonrieger.de>
2025-05-08 18:05:08 +0200

    ci: update test workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest_asyncio
 from crypticorn.common import AuthHandler, BaseUrl
 from typing import AsyncGenerator
+from tests.envs import API_ENV
 
 
 @pytest_asyncio.fixture()
 async def auth_handler() -> AsyncGenerator[AuthHandler, None]:
-    handler = AuthHandler(BaseUrl.LOCAL)
+    handler = AuthHandler(BaseUrl.from_env(API_ENV))
+    assert BaseUrl.from_env(API_ENV) in handler.url
     yield handler
     await handler.client.base_client.close()
