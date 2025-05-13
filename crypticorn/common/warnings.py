f5332de915785bad602b601001f3f8987869d46d
timonrieger <mail@timonrieger.de>
2025-05-13 09:58:51 +0200

    refactor: update type annotations to use Union for compatibility

--- crypticorn/common/warnings.py
+++ crypticorn/common/warnings.py
@@ -1,7 +1,7 @@
 """Crypticorn-specific warnings."""
 
 from __future__ import annotations
-
+from typing import Union
 
 class CrypticornDeprecationWarning(DeprecationWarning):
     """A Crypticorn specific deprecation warning.
@@ -24,7 +24,7 @@ class CrypticornDeprecationWarning(DeprecationWarning):
         message: str,
         *args: object,
         since: tuple[int, int],
-        expected_removal: tuple[int, int] | None = None,
+        expected_removal: Union[tuple[int, int], None] = None,
     ) -> None:
         super().__init__(message, *args)
         self.message = message.rstrip(".")
