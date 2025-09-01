c2430d3cecdadb5d8e665aa8f0d7e9b17102699f
timonrieger <mail@timonrieger.de>
2025-09-01 18:18:18 +0200

    small fixes

--- crypticorn/__init__.py
+++ crypticorn/__init__.py
@@ -1,11 +1,3 @@
-"""
-.. include:: ../README.md
-
-## Versioning
-We adhere to [Semantic Versioning](https://semver.org/).
-You can find the full Changelog [below](#changelog).
-"""
-
 import warnings
 import logging
 from crypticorn.common.logging import configure_logging
